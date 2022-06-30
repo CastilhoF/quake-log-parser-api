c6b45db3c1b7885e776932b671019e644fed88cc
CastilhoF <castilho.wylde@gmail.com>
2022-06-30 16:59:00 -0300

    feat: add parser module to core module.

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,18 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { configEnvironmentsValidation } from '../global/config/env/config.env.schema';
 import { ConfigModule } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 import { winston_config } from '../global/config/log/winston.config';
+import { ParserModule } from '../modules/parser/parser.module';
+import { RequestMiddleware } from '../global/middlewares/request.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.stage.${process.env.STAGE}.env`],
       validationSchema: configEnvironmentsValidation,
+      isGlobal: true,
     }),
     WinstonModule.forRoot(winston_config),
+    ParserModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestMiddleware).forRoutes('/parser/*');
+  }
+}
