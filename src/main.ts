0db803a4ef1493f640c9e1361755ab5e2ddd78e2
CastilhoF <castilho.wylde@gmail.com>
2022-06-29 22:56:21 -0300

    feat: implement swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './core/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './global/config/documentation/swagger.config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
+  // Set the validation pipe and enable CORS
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
+  // Implementation of Swagger
+  const config = swaggerConfig;
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const configService: ConfigService = new ConfigService();
 
+  // Listening on port of application ant start the server
   await app.listen(configService.get('PORT'), configService.get('HOST'));
   logger.log(`Application listening on: ${await app.getUrl()}`);
 }
