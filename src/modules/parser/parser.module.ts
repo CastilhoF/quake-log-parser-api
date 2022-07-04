a31980e15b2a542af0dc5dd357990716f1186322
CastilhoF <castilho.wylde@gmail.com>
2022-07-04 17:00:48 -0300

    feat: change parser module

--- src/modules/parser/parser.module.ts
+++ src/modules/parser/parser.module.ts
@@ -1,12 +1,13 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Helpers } from './helpers/helper';
 import { ParserController } from './parser.controller';
 import { ParserService } from './parser.service';
 
 @Module({
   controllers: [ParserController],
-  imports: [HttpModule, ConfigModule, HttpModule],
-  providers: [ParserService, ConfigService],
+  imports: [HttpModule, ConfigModule],
+  providers: [ParserService, ConfigService, Helpers, Object],
 })
 export class ParserModule {}
